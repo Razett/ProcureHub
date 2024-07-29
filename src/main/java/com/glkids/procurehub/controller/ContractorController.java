924733595684eb004f5e1d5a84ca1311798595d9
jeongjiwon <kzxn4269@gmail.com>
2024-07-29 18:45:16 +0900

    HTMl 템플릿 추가 및 수정 작업 진행 중

--- src/main/java/com/glkids/procurehub/controller/ContractorController.java
+++ src/main/java/com/glkids/procurehub/controller/ContractorController.java
@@ -1,6 +1,10 @@
 package com.glkids.procurehub.controller;
 
+import com.glkids.procurehub.entity.Contractor;
+import com.glkids.procurehub.entity.Quotation;
+import com.glkids.procurehub.entity.QuotationMtrl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,54 +20,59 @@ public class ContractorController {
      * 업체 목록
      */
     @GetMapping("/list")
-    public String List() {
+    public String List(Model model) {
         return "/contractor/list";
     }
 
     /**
      * 업체 상세정보
+     * @param corno 사업자 등록 번호
+     * @return /contractor/get 을 요청하여 업체 상세페이지를 표시합니다.
      */
-    @GetMapping("/cpdetail")
-    public String cpdetail() {
-        return "/contract/cpdetail";
+    @GetMapping("/get")
+    public String get(Long corno, Model model) {
+        return "/contractor/get";
     }
 
     /**
-     * 업체 수정화면에서 상세정보로 가기
-     */
-    @PostMapping("/cpdetail")
-    public String postCpDetail() {
-        // 데이터를 처리하는 로직 추가
-        return "/contract/cpdetail";
-    }
-
-    /**
-     * 업체 수정화면
-     */
-    @GetMapping("/update")
-    public String Listupdate() {
-        return "/contract/update";
-    }
-
-    /**
-     * 업체 등록
+     * 업체 등록 화면
      */
     @GetMapping("/register")
-    public void getRegister() {}
+    public void getRegister() {
+
+    }
 
     /**
-     * 업체 등록 버튼 클릭 시 목록 화면으로
+     * 업체 등록 처리
      */
     @PostMapping("/register")
-    public String postRegister() {
+    public String postRegister(Contractor contractor) {
         return "/contractor/list";
+    }
+
+    /**
+     * 업체 수정 화면
+     */
+    @GetMapping("/update")
+    public String getUpdate(Long corno, Model model) {
+        return "/contractor/update";
+    }
+
+    /**
+     * 업체 수정 처리
+     */
+    @PostMapping("/update")
+    public String postUpdate(Model model, Contractor contractor) {
+        // 데이터를 처리하는 로직 추가
+        model.addAttribute("contractor", contractor);
+        return "/contractor/get?corno=" + contractor.getCorno();
     }
 
     /**
      * 견적 목록
      */
     @GetMapping("/quolist")
-    public String quoList() {
+    public String quoList(Model model) {
         return "/contractor/quolist";
     }
 
@@ -71,13 +80,16 @@ public class ContractorController {
      * 견적 등록
      */
     @GetMapping("/quoregister")
-    public void getQuoRegister() {}
+    public void getQuoRegister(Long corno, Model model) {
+
+    }
 
     /**
-     * 견적 등록 버튼 클릭 시 목록 화면으로
+     * 견적 등록 처리
      */
     @PostMapping("/quoregister")
-    public String postQuoRegister() {
+    public String postQuoRegister(Quotation quotation, QuotationMtrl quotationMtrl) {
+
         return "/contractor/quolist";
     }
 
