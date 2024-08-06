0e4ecbdb2e673c4d0bfe0a6fb07ef1c93af287b5
yoni <yhyh8264@naver.com>
2024-08-06 19:35:41 +0900

    계약등록처리 구현

--- src/main/java/com/glkids/procurehub/controller/AgreementController.java
+++ src/main/java/com/glkids/procurehub/controller/AgreementController.java
@@ -1,16 +1,20 @@
 package com.glkids.procurehub.controller;
 
 import com.glkids.procurehub.dto.*;
-import com.glkids.procurehub.entity.Material;
+import com.glkids.procurehub.entity.*;
 import com.glkids.procurehub.service.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.tags.Param;
+
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /*
  계약 관리 메뉴 컨트롤러
@@ -23,8 +27,7 @@ public class AgreementController {
     private final QuotationService quotationService;
 
     private  final AgreementService agreementService;
-
-    private  final MaterialService materialService;
+    private final ContractorService contractorService;
 
 
     @GetMapping("/register")
@@ -32,13 +35,22 @@ public class AgreementController {
 
         model.addAttribute("qtno", quotationService.read(qtno));
         model.addAttribute("qtnomtrl", quotationService.quotationMtrlList(qtno));
-        model.addAttribute("material", materialService.list());
+
         return "agreement/register";
     }
 
-    @PostMapping("/updatepro")
-    public String proregister(AgreementDTO agreementDTO ){
+    @PostMapping("/register")
+    public String proregister(AgreementDTO agreementDTO, @RequestParam("startdateText") String startdateText, @RequestParam("enddateText") String enddateText) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        agreementDTO.setContractor(Contractor.builder().corno(agreementDTO.getCorno()).build());
+        agreementDTO.setQuotation(Quotation.builder().qtno(agreementDTO.getQtno()).build());
+        agreementDTO.setEmp(Emp.builder().empno(201758030L).build());
+
+        agreementDTO.setStartdate(LocalDate.parse(startdateText, formatter).atStartOfDay());
+        agreementDTO.setEnddate(LocalDate.parse(enddateText, formatter).atStartOfDay());
+
         agreementService.register(agreementDTO);
         return "redirect:/contractor/list";
     }
+
 }
