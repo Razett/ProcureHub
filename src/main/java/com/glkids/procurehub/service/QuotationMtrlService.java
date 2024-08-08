71081f90bd76b8c07d537b8bd2e20a33c9071b64
chung6063 <cuhng6063@gmail.com>
2024-08-08 15:45:49 +0900

    안쓰는기능 삭제

--- src/main/java/com/glkids/procurehub/service/QuotationMtrlService.java
+++
@@ -1,7 +0,0 @@
-package com.glkids.procurehub.service;
-
-import com.glkids.procurehub.entity.QuotationMtrl;
-
-public interface QuotationMtrlService {
-    QuotationMtrl saveQuotationMtrl(QuotationMtrl quotationMtrl);
-}
