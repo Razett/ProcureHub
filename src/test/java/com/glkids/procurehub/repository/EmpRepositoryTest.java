a2ebca0fdd5f9f4345d30d3c0f9675925aa73e2e
diwoong <jiwoochoi127@gmail.com>
2024-08-12 09:16:01 +0900

    발주 현황 작업 중 (발주 대기 현황 수정기능 작업 중)

--- src/test/java/com/glkids/procurehub/repository/EmpRepositoryTest.java
+++ src/test/java/com/glkids/procurehub/repository/EmpRepositoryTest.java
@@ -5,6 +5,7 @@ import com.glkids.procurehub.entity.Emp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -17,9 +18,12 @@ public class EmpRepositoryTest {
     @Autowired
     private DeptRepository deptRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     public void initSave() {
         List<Dept> depts = deptRepository.findAll();
-        empRepository.save(Emp.builder().empno(201758030L).name("정지원").pw("1234").dept(depts.get(0)).build());
+        empRepository.save(Emp.builder().empno(201941049L).name("최지우").pw(passwordEncoder.encode("1127")).dept(depts.get(0)).build());
     }
 }
