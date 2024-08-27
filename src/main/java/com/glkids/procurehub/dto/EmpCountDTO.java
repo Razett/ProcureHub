b49cc251b099cda4b12bbe785c0ac54f2c20f760
yoni <yhyh8264@naver.com>
2024-08-27 20:28:58 +0900

    대시 보드 디자인 수정

--- src/main/java/com/glkids/procurehub/dto/EmpCountDTO.java
+++ src/main/java/com/glkids/procurehub/dto/EmpCountDTO.java
@@ -11,13 +11,15 @@ public class EmpCountDTO {
     private Long empno;          // 사원 번호
     private String name;         // 사원 이름
     private Long importCount;     // 입고 처리 건수
-    private Long exportCount;    // 출고 처리 건수
+    private Long exportCount;
+    private Long orderCount;// 출고 처리 건수
 
     // 필요한 경우, int 타입을 사용하는 생성자 추가 (기존)
-    public EmpCountDTO(Long empno, String name, int importCount, int exportCount) {
+    public EmpCountDTO(Long empno, String name, int importCount, int exportCount, int orderCount) {
         this.empno = empno;
         this.name = name;
         this.importCount = (long) importCount;
         this.exportCount = (long) exportCount;
+        this.orderCount = (long) orderCount;
     }
 }
--- src/main/java/com/glkids/procurehub/repository/EmpRepository.java
+++ src/main/java/com/glkids/procurehub/repository/EmpRepository.java
@@ -11,6 +11,7 @@ public interface EmpRepository extends JpaRepository<Emp, Long> {
     Emp findByEmpno(Long empno);
 
     @Query("SELECT new com.glkids.procurehub.dto.EmpCountDTO(e.empno, e.name, " +
+            "(SELECT COUNT(o) FROM Order o WHERE o.emp.empno = e.empno AND FUNCTION('DATE', o.orderdate) = CURRENT_DATE), " +
             "(SELECT COUNT(i) FROM Imports i WHERE i.approver.empno = e.empno AND FUNCTION('DATE', i.approvedate) = CURRENT_DATE), " +
             "(SELECT COUNT(ex) FROM Export ex WHERE ex.shipper.empno = e.empno AND FUNCTION('DATE', ex.shippeddate) = CURRENT_DATE)) " +
             "FROM Emp e")
