2fc0235178dfd705133327e440d200f03cc782ec
chung6063 <cuhng6063@gmail.com>
2024-08-28 12:14:55 +0900

    순환 참조오류 테스트

--- src/main/java/com/glkids/procurehub/config/SecurityConfig.java
+++ src/main/java/com/glkids/procurehub/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +19,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Lazy
     @Autowired
     private EmpServiceImpl empService;
 
